aa35c4528b604cbfdd153d3c0104481f3fd63e65
i-pichurov <deadbassein@gmail.com>
2024-12-26 17:38:02 +0300

    Update merge_func.py

--- merge/modules/merge_func.py
+++ merge/modules/merge_func.py
@@ -1,9 +1,10 @@
-def merge(first_file, second_file, output_file):
+def merge(first_file, second_file, third_file, output_file):
     file1 = list(map(lambda x: x.rstrip(), open(first_file, 'r')))
     file2 = list(map(lambda x: x.rstrip(), open(second_file, 'r')))
+    file3 = list(map(lambda x: x.rstrip(), open(third_file, 'r')))
     result = open(output_file, 'a+')
 
-    merge_set = set(file1 + file2)
+    merge_set = set(file1 + file2 + file3)
     merge_list = list(merge_set)
     result.write("\n".join(merge_list))
     result.close()
